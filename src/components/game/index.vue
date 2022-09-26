d8a71450c3b9aa694bd2bd22cc5e5059438d4f1b
houxingkuo <544213260@qq.com>
2022-09-26 16:43:44 +0800

    适配高度

--- src/components/game/index.vue
+++ src/components/game/index.vue
@@ -81,7 +81,7 @@ export default {
         top: 0;
         left: 0;
         width: 100%;
-        height: 100vh;
+        height: 100%;
     }
     .loading{
         background-image: url('/static/app/Public/img/game_bg.png');
